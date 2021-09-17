3466f98d8d88162d6901fd807cb33954f2ac63d4
Emo <emotupev11@gmail.com>
2021-09-17 20:52:56 +0300

    feat: added functionality

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,30 +2,54 @@ import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+	static get events() {
+		return {
+			READY: "ready",
+		};
+	}
 
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
+		this._loading = document.querySelector(".progress");
 
-    document.body.querySelector(".main").appendChild(box);
+		this._load();
 
-    this.emit(Application.events.READY);
-  }
+		this.emit(Application.events.READY);
+	}
 
-  _render({ name, terrain, population }) {
-    return `
+	async _load() {
+		const fetchData = await fetch("https://swapi.boom.dev/api/planets");
+		let next = await fetchData.json();
+
+		let counter = 1;
+		while (next !== null) {
+			const fetchPlanets = await fetch(`https://swapi.boom.dev/api/planets?page=${counter}`);
+			const data = await fetchPlanets.json();
+			const planets = await data.results;
+			console.log(planets);
+
+			next = data.next;
+
+			this._startLoading();
+
+			planets.forEach((planet) => {
+				const box = document.createElement("div");
+				box.classList.add("box");
+
+				box.innerHTML = this._create(planet);
+
+				document.body.querySelector(".main").appendChild(box);
+			});
+
+			counter++;
+		}
+		this._stopLoading();
+	}
+
+	_create({ name, terrain, population }) {
+		// renders boxes on screen
+		return `
 <article class="media">
   <div class="media-left">
     <figure class="image is-64x64">
@@ -43,5 +67,15 @@ export default class Application extends EventEmitter {
   </div>
 </article>
     `;
-  }
+	}
+
+	_startLoading() {
+		// animation starts
+		this._loading.style.display = "block";
+	}
+
+	_stopLoading() {
+		this._loading.style.display = "none";
+		// animation stops
+	}
 }
